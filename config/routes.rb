4853b1c6e6352aaf0cc5231f1af7e00e49a6a39c
Collin  Jones <collinpjones@users.noreply.github.com>
2016-10-30 18:37:43 -0500

    Completed Edit

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
 
   # Routes to Delete photos
   get("/delete_photo/:id",  { :controller => "photos", :action => "destroy"})
+
+  #Routes to Update photos
+  get("/photos/:id/edit",       { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id",  { :controller => "photos", :action => "update_row" })
 end
